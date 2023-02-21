a45c7288fd869e3eac5ac28a85fad0f896dd2054
jwj-45 <122075343+jwj-45@users.noreply.github.com>
2023-02-21 15:13:55 +0900

    Update test.java

--- gitprogiect/src/teamCode/test.java
+++ gitprogiect/src/teamCode/test.java
@@ -1,7 +1,7 @@
 package teamCode;
 
-public class Hello {
+public class test {
 
 	public static void main(String[] args) {
-      System.out.println("Test!");
+             System.out.println("Test!");
   }
